e29cf5f91be8c05c8b25f99d478d4253544ee724
Quadri Alameen <quadrameen@gmail.com>
2022-09-03 21:04:31 +0100

    Updates

---
+++ precache-manifest.0c86eaec7e043315495893b44496cc21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "50394ef1d60efb1c157a",
+    "url": "/Notes-app/static/css/main.618af4cb.chunk.css"
+  },
+  {
+    "revision": "50394ef1d60efb1c157a",
+    "url": "/Notes-app/static/js/main.50394ef1.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/Notes-app/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "ebdb3938944536eb4768",
+    "url": "/Notes-app/static/css/2.84442ff2.chunk.css"
+  },
+  {
+    "revision": "ebdb3938944536eb4768",
+    "url": "/Notes-app/static/js/2.ebdb3938.chunk.js"
+  },
+  {
+    "revision": "6dd35f147ba09df671a1",
+    "url": "/Notes-app/static/js/runtime~main.6dd35f14.js"
+  },
+  {
+    "revision": "e83e26e9e2ac0a62b20e221c1867a7b7",
+    "url": "/Notes-app/index.html"
+  }
+];
